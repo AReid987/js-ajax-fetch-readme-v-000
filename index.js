6b3b823d8006d176af60712a0234489066028faf
Antonio Reid <read.musik@gmail.com>
2018-06-28 15:54:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = "I don't do much.";
-const token = '05edc866eeeea746dd6346c1509cb2135e6e0ef7'
+const token = 
 fetch('https://api.github.com/user/repos', {
   headers: {
     Authorization: `token ${token}`
